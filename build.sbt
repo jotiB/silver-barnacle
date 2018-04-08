bb407557675e0d4d27aaf29978dcbfa90eef704a
jyotinbiswas <jbiswas@verax.ca>
2018-04-07 20:05:46 -0400

    Working Programs v2 with temp tables / thrift server jar files

--- build.sbt
+++ build.sbt
@@ -6,11 +6,11 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.1" % "provided"
 
-libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.1.1"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.1.1"  % "provided"
 
 libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.1.1"
 
-libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.2.1"
+libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "0.10.2.1" %  "provided"
 
 libraryDependencies += "org.apache.spark" % "spark-sql-kafka-0-10_2.11" % "2.1.0"
 
@@ -18,5 +18,5 @@ libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10-assembly
 
 libraryDependencies += "org.apache.spark" % "spark-hive_2.11" % "2.1.1"
 
-libraryDependencies += "org.apache.spark" % "spark-hive-thriftserver_2.11" % "2.1.1"
+libraryDependencies += "org.apache.spark" % "spark-hive-thriftserver_2.11" % "2.1.1" %  "provided"
 
--- src/main/scala/StructuredStreamingConsumer.scala
+++ src/main/scala/StructuredStreamingConsumer.scala
@@ -1,11 +1,40 @@
 import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
 import org.apache.spark.sql.functions.from_json
+import org.apache.spark.sql.functions.from_unixtime
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.streaming.OutputMode
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.hive.thriftserver._
+import org.apache.spark.sql.types._
+
+
+
+/* To run the code snippets of this program in Zeppelin, change the following config values
+Zeppelin Spark Interpreter:
+master = yarn
+Dependencies
+org.apache.kafka:kafka-clients:0.10.1.1
+org.apache.spark:spark-streaming-kafka-0-10_2.11:2.1.1
+org.apache.kafka:kafka_2.11:0.10.1.1
+
+Advanced Zeppelin Config in Ambari Zeppelin configs zeppelin_env_content
+export MASTER=yarn
+export SPARK_HOME=/usr/hdp/current/spark2-client
+export SPARK_SUBMIT_OPTIONS="--packages   org.apache.spark:spark-sql-kafka-0-10_2.11:2.1.0,
+org.apache.spark:spark-sql_2.11:2.1.0,org.apache.kafka:kafka_2.11:0.10.0.1,
+org.apache.spark:spark-streaming-kafka-0-10_2.11:2.1.0,org.apache.kafka:kafka-clients:0.10.0.1"
+*/
 
 object StructuredStreamingConsumer {
+  // Stock Quotes Schema creation
+  case class StockQuote(exchange: String,
+                        ticker: String,
+                        lastTradeDate: Long,
+                        open: Float,
+                        high: Float,
+                        low: Float,
+                        close: Float,
+                        volume: Float)
 
   def main(args: Array[String]) {
 
@@ -14,8 +43,12 @@ object StructuredStreamingConsumer {
       .appName("StructuredStreamingConsumer")
       .enableHiveSupport()
       .getOrCreate()
+
     val sqlContext = spark.sqlContext
+    sqlContext.setConf("hive.server2.thrift.port","10016")
+
     HiveThriftServer2.startWithContext(sqlContext)
+
     /*
   Kafka Schema
 root
@@ -41,40 +74,35 @@ root
  ))*/
 
 
-    /* Stock Quotes Schema creation DSL method
-    val stockQuotesSchema = new StructType()
+     //Stock Quotes Schema creation DSL method
+    val schema = new StructType()
     .add("exchange", StringType)
     .add("ticker", StringType)
-    .add("lastTradeDate", TimestampType)
+    .add("lastTradeDate", LongType)
     .add("open", FloatType)
     .add("high", FloatType)
     .add("low", FloatType)
     .add("close", FloatType)
-    .add("volume", FloatType)*/
+    .add("volume", FloatType)
 
-    // Stock Quotes Schema creation best practice method
-    case class StockQuote(exchange: String,
-                          ticker: String,
-                          lastTradeDate: java.sql.Timestamp,
-                          open: Float,
-                          high: Float,
-                          low: Float,
-                          close: Float,
-                          volume: Float)
 
-    val schema = Encoders.product[StockQuote].schema
+
+    //val schema = Encoders.product[StockQuote].schema
+    //val stockQuoteTag = typeTag[StockQuote];
+    //val tt = typeTag[StockQuote];
+    //val schema = Encoders.product[StockQuote]
 
     // Use Spark DSL to create the quotes base data frame
 
     val quotesDF = spark
       .readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", "developmentBox.hadoop.verax.ca:9092")
-      .option("subscribe", "stock-quotes")
+      .option("kafka.bootstrap.servers", "devbox.hdpdev.verax.ca:9092")
+      .option("subscribe", "market-quotes")
       .option("startingOffsets", "earliest")
       .load()
       .withColumn("quote", from_json(col("value").cast("string"), schema))
-      .select(col("quote.exchange"), col("quote.ticker"), col("quote.lastTradeDate"),
+      .select(col("quote.exchange"), col("quote.ticker"), (col("quote.lastTradeDate")/1000).cast(TimestampType).alias("lastTradedTs"),
         col("quote.open"), col("quote.high"), col("quote.low"), col("quote.close"), col("quote.volume")
       )
 
@@ -87,22 +115,18 @@ root
         .saveAsTable("quotes")
       System.out.println(baseQueryDF)
     }*/
+    spark.sql("select exchange, ticker, lastTradedTs, high, low, open, close, volume from quotes").writeStream.format("console").start()
 
-    spark.sql("select exchange, ticker, lastTradeDate, high, low, open, close, volume from quotes")
-      .show()
-
-    val stocks2017DS = quotesDF.filter("year(lastTradeDate)==2017")
+    val stocks2017DS = quotesDF.filter("year(lastTradedTs)==2017")
     // Use Spark DSL to create the quotes analysis data frame
 
-
-
-    val slidingWindowDS = stocks2017DS.groupBy(window(col("quote.lastTradeDate"), "3 minutes", "30 seconds"))
-      .agg(sum("volume").as("trade_volume"))// window duration and slide duration specified
+    val slidingWindowDS = stocks2017DS.groupBy(window(col("lastTradedTs"), "1 day", "60 minutes"))
+      .agg(sum("volume").as("trade_volume").cast(FloatType))// window duration and slide duration specified
 
     printWindow(slidingWindowDS, "trade_volume")
 
-    val tumblingWindowDS = stocks2017DS.groupBy(window(col("quote.lastTradeDate"), "1 day"))
-      .agg(sum("volume").as("trade_volume")) // Only Window duration specified
+    val tumblingWindowDS = stocks2017DS.groupBy(window(col("lastTradedTs"), "1 day"))
+      .agg(sum("volume").as("trade_volume").cast(FloatType)) // Only Window duration specified
 
     printWindow(tumblingWindowDS, "trade_volume")
 
@@ -111,7 +135,7 @@ root
 
     val dumpQuery = quotesDF.writeStream.format("console").start()
 
-    val quotesAnalysis = quotesDF.groupBy("ticker" ).sum("volume")
+    val quotesAnalysis = quotesDF.groupBy("ticker").sum("volume")
     val aggQuery = quotesAnalysis.
       writeStream.
       outputMode(OutputMode.Complete).
